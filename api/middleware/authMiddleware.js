6968d3b94c0009cbe7035c35e9f60f0dbac4118f
caffeine23 <hellcrusher23@protonmail.com>
2024-07-09 19:10:29 +0530

    added auth middleware

---
+++ api/middleware/authMiddleware.js
@@ -0,0 +1,19 @@
+// uncomment after frontend is ready
+// const jwt = require("jsonwebtoken");
+
+// const verifyToken = (req, res, next) => {
+//   const header = req.headers.authorization;
+//   if (!header) {
+//     return res.status(401).send("Unauthorized - No token provided");
+//   }
+//   const token = header.replace("Bearer ", "");
+//   try {
+//     const decodedToken = jwt.verify(token, process.env.JWT_KEY);
+//     req.userId = decodedToken.userId;
+//     next();
+//   } catch (err) {
+//     return res.status(401).send("Unauthorized - Invalid token");
+//   }
+// };
+
+// module.exports = verifyToken;
