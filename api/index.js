9cf133cfef0087e0663bba4e07de5639de2fe74c
caffeine23 <hellcrusher23@protonmail.com>
2024-07-09 19:11:10 +0530

    added routes for auth, post, and comment

--- api/index.js
+++ api/index.js
@@ -3,6 +3,9 @@ const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const cors = require("cors");
 
+const authRoutes = require("./routes/authRoutes");
+const postRoutes = require("./routes/postRoutes");
+const commentRoutes = require("./routes/commentRoutes");
 const errorHandler = require("./utils/errorHandler");
 
 dotenv.config();
@@ -13,6 +16,10 @@ const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cors());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/post", postRoutes);
+app.use("/api/comment", commentRoutes);
 
 app.get("/test", (req, res) => {
   res.send("Server is up.");
