5a36dbaaf7513d6be2808e7115e87206c24b5c04
caffeine23 <hellcrusher23@protonmail.com>
2024-07-15 16:17:27 +0530

    added router and chakra ui

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,36 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './pages/App'
-import './styles/index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./pages/App.tsx";
+import "./styles/index.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import axios from "axios";
+import Login from "./pages/Login.tsx";
+import Register from "./pages/Register.tsx";
+import ErrorPage from "./pages/ErrorPage.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+axios.defaults.baseURL = "http://localhost:3000/api";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ChakraProvider>
+        <RouterProvider router={router} />
+    </ChakraProvider>
+  </React.StrictMode>
+);
